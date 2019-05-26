076a17edbcd845f75d0028ff175e9842b8f5592a
keyhundred <dls0901@naver.com>
2019-05-26 21:58:56 +0900

    git commit

--- headerAnalyzer.java
+++ headerAnalyzer.java
@@ -1,9 +1,31 @@
-package proj;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 class headerAnalyzer{
-    public headerAnalyzer()
+    File header = null;
+    public headerAnalyzer(String filename)
     {
-        ;
+        header = new File(filename);
+    }
+
+    public void analyze()
+    {
+        try
+        {
+            FileReader headReader = new FileReader(header);
+        }
+        catch(FileNotFoundException fe)
+        {
+            fe.printStackTrace();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        //if property related with configuration, call function associated.
+        //if not, convert it to useful information.
     }
 }
