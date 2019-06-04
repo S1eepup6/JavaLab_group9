337a280068b0fe89cc5d1e0d6138722d347db129
keyhundred <dls0901@naver.com>
2019-06-04 11:49:22 +0900

    Emotion ready v.2

--- Main.java
+++ Main.java
@@ -4,8 +4,14 @@ class Main
 {
     public static void main(String[] args) {
         emotionReady mainER = new emotionReady("emotion.txt");
-        String msg = "I was born to luv u with every single beats of my hearts.";
+        String msg = "I was born to love you with every single beats of my hearts.";
         messagePrint analyzer = new messagePrint("header.txt",msg,"8888",mainER);
-        analyzer.printMSG();
+        analyzer.printMessage();
+        msg = "I was born to take care of you, honey!";
+        analyzer.getNewMessage(msg);
+        analyzer.printMessage();
+        msg = "With every single days of my life!";
+        analyzer.getNewMessage(msg);
+        analyzer.printMessage();
     }
 }
